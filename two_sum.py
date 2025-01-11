21f4e93f71ef74f9f96b8ffe2d9841d1dcb2162e
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2025-01-10 23:18:44 -0600

    Solutions to two sum problem

--- two_sum.py
+++ two_sum.py
@@ -1,21 +1,54 @@
 from typing import List
 
+'''My firts Solution'''
+# class Solution:
+#   def twoSum(self, nums: List[int], target: int) -> List[int]:
+#     i = 0
+#     j = 0
 
+#     while i < len(nums):
+      
+#       while j < len(nums):
+#         if nums[i] + nums[j] == target and i != j:
+#           return [i, j]
+#         j += 1
+      
+#       i += 1
+#       j = 0
+
+'''My second solution'''
+# class Solution:
+#   def twoSum(self, nums: List[int], target: int) -> List[int]:
+#     self.nums = nums
+#     self.target = target
+#     self.recursive(0, 0,)
+
+#     return self.sol
+  
+#   def recursive(self, v_act_index, c_index):
+    
+#     if c_index < len(self.nums) and self.nums[v_act_index] + self.nums[c_index] == self.target and v_act_index != c_index :
+#       self.sol = [v_act_index, c_index]
+#       return
+    
+#     if c_index >= len(self.nums):
+#       self.recursive((v_act_index + 1), 0 )
+#     else:
+#       self.recursive( v_act_index, (c_index + 1))
+
+'''Optimal solution'''
 class Solution:
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    i = 0
-    j = 0
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        processMap = {}
 
-    while i < len(nums):
-      
-      while j < len(nums):
-        if nums[i] + nums[j] == target and i != j:
-          return [i, j]
-        j += 1
-      
-      i += 1
-      j = 0
+        for i, v in enumerate(nums):
+            if target-v in processMap:
+                return [processMap[target-v], i]
+            
+            processMap[v] = i
 
 sol = Solution()
-print(sol.twoSum([3,2,4], 6))
+print(sol.twoSum([3,1,3], 6))
 
+# ¿Que necesito saber?
+# index actual, self.nums, target
